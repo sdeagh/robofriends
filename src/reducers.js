cdd00f380853b477e7556a9e21256f804afc1dac
sdeagh <huw.burton@btinternet.com>
2018-05-15 20:42:02 +0100

    finishing adding redux

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,38 @@
-export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD'
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED,
+} from './constants.js'
 
-const initialState = {
+const initialStateSearch = {
     searchField: ""
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, { searchField: action.payload })
             // OR return (...state, {searcField: action.payload})
+        default:
+            return state
+    }
+}
+
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ""
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+    switch(action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, {isPenfing: true})
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, {robots: action.payload, isPending: false})
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, {error: action.payload, isPending: false})
         default:
             return state
     }
